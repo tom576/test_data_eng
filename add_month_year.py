00ea2fa4bd2bc0710ad4fc6a73f0cbad1575ee6a
Tom <tom.saunders576@gail.com>
2020-02-09 14:13:07 +0000

    add_month_year.py modified

--- add_month_year.py
+++ add_month_year.py
@@ -1,9 +1,16 @@
 import pandas
 
-df = pandas.read_csv('sample.csv')
+def process_file():
+	df = pandas.read_csv('sample.csv')
 
-df['Day'] = pandas.to_datetime(df['Day'])
-df['Month_Year'] = df['Day'].dt.to_period('M')
+	df['Day'] = pandas.to_datetime(df['Day'])
+	df['Month_Year'] = df['Day'].dt.to_period('M')
 
-df.to_csv(r'sampleoutput.csv', index = None, header = True)
+	#df.to_csv(r'sampleoutput.csv', index = None, header = True)
+
+	return df
+
+
+
+
 
